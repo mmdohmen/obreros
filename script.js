0c4dfdbb1ac56066a214e091dcb5c76775541fa9
mmdohmen <mmdohmen@hotmail.com>
2024-09-05 15:29:53 -0300

    obras.json

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function obra() {
     let url = 'obra.html';
     window.location.href = url;
 }
+
+url = "https://raw.githubusercontent.com/mmdohmen/obreros/main/data.js"
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    // Aquí puedes trabajar con los datos del JSON
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
