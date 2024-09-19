392bdd0da9a74d2a4416f3666773487466e3836b
mmdohmen <mmdohmen@hotmail.com>
2024-09-19 11:32:14 -0300

    filtrar x zona

--- obras.js
+++ obras.js
@@ -13,18 +13,31 @@ h1categoria.innerText = categoria.toUpperCase()
 // capturo al contenedor del DOM
 const eventosDOM = document.getElementById("obras")
 
+// capturo los checkboxs de ZONAS
+const zonas = document.getElementById("zonas")
+console.error('zonas:', zonas)
 
-// datos locales =================================================================================
-console.log("datos LOCALES:   obras.data")
-const datosObras = obras.data
-console.log("datosObras: ", datosObras)
-console.log(typeof (datosObras))
+// CAPTURO todos los CHECKBOX en una NodeList() o coleccion de NODOS (objetos)
+// capturar los checkbox
+let checkboxes = document.querySelectorAll("input[type='checkbox']")
+console.log('checkboxes: ', checkboxes)
+// TRANSFORMO la NodeList() en un ARRAY
+let arrayCheckboxes = Array.from(checkboxes)
+console.log("array de checkboxes:", arrayCheckboxes)
+
+
+// DATOS locales =================================================================================
+//console.warn("datos LOCALES:   obras.data")
+//const datosObras = obras.data
+//console.log("datosObras: ", datosObras)
+//console.log(typeof (datosObras))
 
 // llamo a la funcion
 //cards(datosObras)
 
 
-// datos web =====================================================================================
+
+// DATOS web =====================================================================================
 url = "https://raw.githubusercontent.com/mmdohmen/obreros/main/obras.json"
 
 fetch(url)
@@ -36,13 +49,46 @@ fetch(url)
   })
   .then(obras => {
     console.error("datos WEB:   obras.json")
-    console.log(obras);
+    console.log('obras WEB: ', obras);
     // Aquí puedes trabajar con los datos del JSON
     console.log(typeof (obras))
     console.warn('obras.data: ', obras.data)
-    console.log(typeof (obras))
+    console.log(typeof (obras.data))
 
-    cards(obras.data)
+    let arrayObras = obras.data
+    console.log('obras: ', arrayObras)
+
+    // FILTRO los objetos segun la CATEGORIA "categoria" elegida
+    // uso filter() para: iterar sobre cada objeto en el array 'arrayDatos'
+    //     some()         verificar si al menos uno de los elementos del array categorias es igual a "categoria"
+    let arrayObrasFiltradoCategoria = arrayObras.filter(item => item.categorias.some(cat => cat === categoria))
+    console.log('obras filtradas x categoria: ', arrayObrasFiltradoCategoria)
+    cards(arrayObrasFiltradoCategoria)
+
+    // habilito un EVENTO que muestre las obras segun su ZONA => coloco un eventlistener
+    //    dentro del 'eventListener':   1. modifico el array de obras segun las zonas seleccionadas
+    //                                  2. renderizo el array de obras por zona
+    zonas.addEventListener('change', () => {
+      let checkboxesCheckeados = arrayCheckboxes.filter(elemento => elemento.checked)
+      console.log("checkboxes Checkeados:", checkboxesCheckeados)
+
+      // si NO HAY elementos CHECKED renderizo todas las obras
+      if (checkboxesCheckeados.length == 0) {
+        cards(arrayObrasFiltradoCategoria)
+      }
+
+      if (checkboxesCheckeados.length != 0) {
+        console.log('aplicar FILTRO x zona')
+        // armo un ARRAY con el NOMBRE/value de las ZONAS q estan CHECKED
+        let zonasCheckeadas = checkboxesCheckeados.map(elemento => elemento.value)
+        console.log("ZONAS Checkeadas:", zonasCheckeadas)
+        // armo el ARRAY de DATOS que tienen las ZONAS CHECKEaDas
+        arrayDatos = arrayObrasFiltradoCategoria.filter(elemento => zonasCheckeadas.includes(elemento.zona))
+        console.log("obras segun zona: ", arrayDatos)
+        cards(arrayDatos)
+      }
+
+    })
 
   })
   .catch(error => {
@@ -51,24 +97,22 @@ fetch(url)
 
 
 
-// FUNCIONES   ================================================================================================================================
+// FUNCIONES   ============================================================================================
 function cards(arrayDatos) {
+
+  console.warn("cards")
+
   if (arrayDatos.length == 0) {
     eventosDOM.innerHTML = ""
     Swal.fire('there are NO EVENTS with the text entered ...').then(resultado => {
       if (resultado.value) { window.location.reload() }
     })
   }
+
   let cards = ""
 
-  // Filtrar los objetos con la categoria "categoria"
-  // uso filter() para: iterar sobre cada objeto en el array 'arrayDatos'
-  //     some()         verificar si al menos uno de los elementos del array categorias es igual a "categoria"
-  const arrayDatosFiltrado = arrayDatos.filter(item => item.categorias.some(cat => cat === categoria))
+  arrayDatos.forEach(element => {
 
-  arrayDatosFiltrado.forEach(element => {
-    //if (element.categorias == categoria)}
-    console.log("if funcionando", categoria)
     cards += `
         <div class="row obra" >
             <div class="col-4">
@@ -87,9 +131,12 @@ function cards(arrayDatos) {
             </div>
         </div>
         `
-    //}
   });
+
   eventosDOM.innerHTML = cards
+
 }
+
+
 
 
